92fe6cbbc2b64ffd0bdf259cbbf4239f491c2804
subhash-multicloud-devops <subhashregati2@gmail.com>
2026-09-13 14:31:08 +0530

    dyanamo-db added

--- backend.tf
+++ backend.tf
@@ -3,5 +3,7 @@ backend "s3" {
     bucket       = "backend-s3-979179610386-ap-south-2-an"
     key          = "prod/terraform.tfstate"
     region       = "ap-south-2"
+    # use_lockfile = true
+    dynamodb_table = "terraform-state-lock"
     }
 }
